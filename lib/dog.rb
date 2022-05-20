c0c01263dcf0774cc2259efbc107ff990af8a6bf
Taylor Huffman <taylor@crucialartists.com>
2022-05-20 17:57:26 -0500

    Complete bonus

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,4 +79,32 @@ class Dog
         end.first
     end
 
+    def self.find_or_create_by(name:, breed:)
+        sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+            AND breed = ?
+            LIMIT 1
+        SQL
+        row = DB[:conn].execute(sql, name, breed).first
+
+        if row
+            self.new_from_db(row)
+        else
+            self.create(name: name, breed: breed)
+        end
+    end
+
+    def update
+        sql = <<-SQL
+            UPDATE dogs
+            SET
+                name = ?,
+                breed = ?
+            WHERE id = ?;
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
 end
